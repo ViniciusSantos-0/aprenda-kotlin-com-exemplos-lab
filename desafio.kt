c09bce724f8a31b9464ea81f34f519176ff7b8bf
Vinicius Santos Silva <55768583+ViniciusSantos-0@users.noreply.github.com>
2023-04-05 16:04:54 -0300

    Update desafio.kt
    Desafio concluído.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,116 @@
+
+@ViniciusSantos-0  
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {
+    BASICO,
+    INTERMEDIARIO,
+    AVANCADO
+}
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+data class Usuario(val nomeUsuario: String, val matriculaUsuario: String) {
+    override fun toString(): String {
+        return "$nomeUsuario (matrícula $matriculaUsuario)"
     }
 }
 
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60) {
+    override fun toString(): String {
+        return "$nome (Duração: $duracao)"
+    }
+}
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+
+    val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(vararg usuario: Usuario) {
+        inscritos.addAll(usuario)
+    }
+
+    override fun toString(): String {
+        //  return "Formação: $nome\nConteúdo educacional: $conteudos\nNível: $nivel\nAlunos
+        // inscritos na formação: $inscritos"
+        return """
+        Formação: $nome
+        Conteúdo educacional: $conteudos
+        Duração do conteúdo: 
+        Nível: $nivel
+        Alunos inscritos na Formação: $inscritos
+        """.trimIndent()
+    }
+}
+
+fun generateMatricula(): String {
+    return (1000..10000).random().toString()
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //  TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de
+    // evoluí-las.")
+    //  TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em
+    // questão.")
+    //
+    //
+    val vinicius = Usuario("Vinicius", generateMatricula())
+    val jake = Usuario("Jake", generateMatricula())
+    val joao = Usuario("Joao", generateMatricula())
+    val bia = Usuario("Bia", generateMatricula())
+    val daniel = Usuario("Daniel", generateMatricula())
+    val pedro = Usuario("Pedro", generateMatricula())
+    val camila = Usuario("Camila" generateMatricula())
+    val conteudoEducacionalAngular = ConteudoEducacional("Formação Angular Developer", 36)
+    val conteudoEducacionalTypeScript =
+            ConteudoEducacional("Formação TypeScript Fullstack Developer", 44)
+    val conteudoEducacionalReact = ConteudoEducacional("Formação React Developer", 34)
+    val conteudoEducacionalJava = ConteudoEducacional("Java", 150)
+    val conteudoEducacionalKotlin = ConteudoEducacional("Kotlin", 120)
+    val conteudoEducacionalSpring = ConteudoEducacional("Spring Framework", 100)
+    val conteudoEducacionalFlutter = ConteudoEducacional("Flutter", 90)
+    val formacaoJava =
+            Formacao("Java Developer", listOf(conteudoEducacionalJava), Nivel.INTERMEDIARIO)
+    val formacaoReact =
+            Formacao("React Developer", listOf(conteudoEducacionalReact), Nivel.INTERMEDIARIO)
+    val formacaoKotlin =
+            Formacao("Formação Kotlin Developer", listOf(conteudoEducacionalKotlin), Nivel.BASICO)
+    val formacaoTypeScript =
+            Formacao(
+                    "Formação TypeScript Developer",
+                    listOf(conteudoEducacionalTypeScript),
+                    Nivel.INTERMEDIARIO
+            )
+    val formacaoSpring =
+            Formacao(
+                    "Spring Framework Developer",
+                    listOf(conteudoEducacionalSpring),
+                    Nivel.INTERMEDIARIO
+            )
+    val formacaoWeb =
+            Formacao(
+                    "Formação Web Developer",
+                    listOf(
+                            conteudoEducacionalAngular,
+                            conteudoEducacionalTypeScript,
+                            conteudoEducacionalReact
+                    ),
+                    Nivel.AVANCADO
+            )
+    val formacaoFlutter =
+            Formacao("Formação Flutter Developer", listOf(conteudoEducacionalFlutter), Nivel.BASICO)
+    formacaoJava.matricular(marcelo, antony)
+    formacaoWeb.matricular(marcelo, vitoria, zane)
+    formacaoKotlin.matricular(joao, antony)
+    formacaoReact.matricular(raquel, nycolle, zane)
+    formacaoTypeScript.matricular(raquel, nycolle)
+    println(formacaoJava)
+    println("\n**********************\n")
+    println(formacaoWeb)
+    println("\n**********************\n")
+    println(formacaoKotlin)
+    println("\n**********************\n")
+    println(formacaoReact)
+    println("\n**********************\n")
+    println(formacaoTypeScript)
+    println("\n**********************\n")
 }
